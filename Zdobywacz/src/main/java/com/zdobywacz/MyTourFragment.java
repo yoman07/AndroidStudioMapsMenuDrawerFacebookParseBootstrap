ea92ae33426f865a9c0fd4365bb69b9ac1eb3f01
Roman Barzyczak <roman.barzyczak@zadane.pl>
2013-11-05 23:14:54 +0100

    test

--- Zdobywacz/src/main/java/com/zdobywacz/MyTourFragment.java
+++ Zdobywacz/src/main/java/com/zdobywacz/MyTourFragment.java
@@ -34,8 +34,8 @@ public class MyTourFragment extends ListFragment {
         super.onActivityCreated(savedInstanceState);
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
-        
+                "Linux", "OS/2", "test" };
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
